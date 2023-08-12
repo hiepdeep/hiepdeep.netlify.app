aaab75be5e275fb22092b8d47ab314aebcb401a5
HiepDeep <vanhiepofficial@gmail.com>
2023-08-12 23:26:46 +0700

    Update tracks.js

--- project/zingMp3/tracks.js
+++ project/zingMp3/tracks.js
@@ -4,51 +4,47 @@ let tracks = [
 	// 	"key": ""
 	// },
 	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Cam-On-Vo-Tong-Gia-Vy/ZW7U9DCO.html",
-		"key": "ZmxnykHNQWBLHsptnyvnZntLCxbQBDpnR"
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Dung-Ai-Nhac-Ve-Co-Ay-Pham-Anh-Quan/ZZBBIDBB.html",
+		"key": "ZHJmtLkdmSJdnmBtmtbmZnyZhRLcCsQDL"
 	},
 	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Chi-Bang-Cai-Gat-Dau-Yan-Nguyen/ZW7U8OU9.html",
-		"key": "ZHJHykmaQpDzbRsynyFHknykgxvQdvXdn"
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Em-Gi-Oi-Jack-K-ICM/ZWAEFWIF.html",
+		"key": "LGcHykmsEBpBaWiyHtFHkmyZXRkxCahas"
 	},
 	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Cam-On-Ton-Thuong-Pham-Nguyen-Ngoc-BMZ/ZWC0C88D.html",
-		"key": "ZmJmtLHxmpvbsCQtmtFHkHykhcFpBDXhR"
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Neu-La-Anh-The-Men/ZW67D7O8.html",
+		"key": "ZmcmtkGsSCuVckhyGTvHLHtZhiZJCQdJN"
 	},
 	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Chang-The-Voi-Lay-Pham-Nguyen-Ngoc-BMZ/ZWBOW7FW.html",
-		"key": "LHcmtLHsiCbiCxvtmtbHkGtLCJDQdFXiN"
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Ngay-Mai-Nguoi-Ta-Lay-Chong-Thanh-Dat/Z6AABFU6.html",
+		"key": "LmxnyLZlhZJaACvyGyvnknTkgELJgNsga"
 	},
 	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Con-Trai-Cung-K-ICM-B-Ray/ZW9FU0A0.html",
-		"key": "kGxnyZmNxBFpDcnTHTvHLHtZgJFQBFNFW"
+		"url": "view-source:https://mp3.zing.vn/bai-hat/That-Tinh-Trinh-Dinh-Quang/ZW7W9A9C.html",
+		"key": "ZGcmyZHaQdRiLugymyFGkHyZhRkcCNaRs"
+	},
+	{
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Danh-Mat-Em-Quang-Dang-Tran/ZWDBBADB.html",
+		"key": "LncHyZGcFFJJaBLTGybHZmyZCiLJCscFS"
+	},
+	{
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Xin-Mot-Lan-Ngoai-Le-Cover-Trinh-Dinh-Quang/ZWABO607.html",
+		"key": "kmJmyZmNELHcuuiTnyDnLnykCiZJhNRQd"
+	},
+	{
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Mua-Trong-Long-Trinh-Dinh-Quang/ZW787CFI.html",
+		"key": "ZnxntZGsQacScZNymybHZHyLCEZJhaicV"
+	},
+	{
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Neu-Khong-The-Den-Voi-Nhau-Trinh-Dinh-Quang/ZW6FIBAW.html",
+		"key": "ZGcmyLHaQkamGJbtmyFnLHykCEZxgcmFd"
+	},
+	{
+		"url": "view-source:https://mp3.zing.vn/bai-hat/Em-Giau-Anh-Dieu-Gi-Trinh-Dinh-Quang/ZWAD6DIO.html",
+		"key": "kHxHTZnsuDWlZCVTnTDGkmTkhiZJCcnCm"
 	},
 	{
 		"url": "view-source:https://mp3.zing.vn/bai-hat/Neu-Phai-Mat-Nhau-Dinh-Dung/Z6W876Z7.html",
 		"key": "ZGcmyLkVsClRLZLtHyDmZGtZhJDWVvAVJ"
-	},
-	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Ho-Dau-Thuong-Em-Phan-Duy-Anh-Ngan-Ngan-ACV/Z6600ZCO.html",
-		"key": "LHJGtZLlLFEmBHaTHTbHZHtZCcbQdDmAB"
-	},
-	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Yeu-Em-Dai-Kho-Cover-Le-Minh-Hieu/ZWBWZDE8.html",
-		"key": "ZGxnykHaipNaihmtHtbGZmtLCxDWBvQgi"
-	},
-	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Tha-La-Khai-Dang/ZZ8D9UZI.html",
-		"key": "ZHcmtLZvsSuJamptGyvGLmykgNRNaCLXS"
-	},
-	{
-		"url": "view-source:https://mp3.zing.vn/Ngay-Mai-Em-Di-Mat-Khai-Dang-Dat-G/ZZEF769O.html",
-		"key": "ZnxHtLZBBiZBgZEymyvmZmTkhNJGdphkN"
-	},
-	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Tinh-Yeu-Co-Con-Ton-Tai-Khai-Dang/ZZ90FZWC.html",
-		"key": "kGxHyZLbNsFHZHxTntDGZmtLCsRgsNcxk"
-	},
-	{
-		"url": "view-source:https://mp3.zing.vn/bai-hat/Thay-Toi-Yeu-Co-Ay-Thanh-Hung/ZWAEU0EE.html",
-		"key": "LnxGtLmsRBHcduxynyFmkHyZXaJnBRNFz"
 	}
 ]
